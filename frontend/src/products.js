7999bf5712ab7193ac12fedc2b18e69d9c2dd9f0
Mechack Ira <mechackniyomukiza@gmail.com>
2022-03-12 13:40:38 -0500

    added data template,frontend and backend folders

---
+++ frontend/src/products.js
@@ -0,0 +1,84 @@
+const products = [
+    {
+      '_id': '1',
+      'name': 'Michael Jackson Off the wall',
+      'image': '/images/mj.jpg',
+      'description':
+        'Off the Wall is the fifth solo studio album by American singer Michael Jackson, released on August 10, 1979, by Epic Records',
+      'genre': 'Disco',
+      'category': 'CD',
+      'price': 54.99,
+      'countInStock': 10,
+      'rating': 4.5,
+      'numReviews': 12,
+    },
+    {
+      '_id': '2',
+      'name': 'The real Bill Withers',
+      'image': '/images/billWithers.jpg',
+      'description':
+        'William Harrison Withers Jr. was an American singer-songwriter and musician. He had several hits over a career spanning fifteen years, including "Use Me", "Lean on Me", "Lovely Day" and "Just the Two of Us".',
+      'genre': 'Soul',
+      'category': 'CD',
+      'price': 19.99,
+      'countInStock': 0,
+      'rating': 4.0,
+      'numReviews': 8,
+    },
+    {
+      '_id': '3',
+      'name': 'The Diary of Alicia Keys',
+      'image': '/images/keys.jpg',
+      'description':
+        'The Diary of Alicia Keys is the second studio album by American singer Alicia Keys. It was released on December 1, 2003, by J Records.',
+      'genre': 'Neo-soul',
+      'category': 'CD',
+      'price': 29.99,
+      'countInStock': 5,
+      'rating': 3,
+      'numReviews': 12,
+    },
+    {
+      '_id': '4',
+      'name': 'The Chronic',
+      'image': '/images/dre.jpg',
+      'description':
+        'The Chronic is the debut studio album by American hip hop producer and rapper Dr. Dre. It was released on December 15, 1992, by his own record label Death Row Records and distributed by Interscope Records.',
+      'genre': 'Rap',
+      'category': 'Cassette',
+      'price': 9.99,
+      'countInStock': 11,
+      'rating': 5,
+      'numReviews': 12,
+    },
+    {
+      '_id': '5',
+      'name': 'Kind of blue',
+      'image': '/images/miles.jpg',
+      'description':
+        'Kind of Blue is a studio album by American jazz trumpeter, composer, and bandleader Miles Davis. It was recorded on March 2 and April 22, 1959, at Columbias 30th Street Studio in New York City, and released on August 17 of that year by Columbia Records.',
+      'genre': 'Jazz',
+      'category': 'Vinyl',
+      'price': 49.99,
+      'countInStock': 7,
+      'rating': 3.5,
+      'numReviews': 10,
+    },
+    {
+      '_id': '6',
+      'name': 'My Beautiful Dark Twisted Fantasy',
+      'image': '/images/west.jpg',
+      'description':
+        'My Beautiful Dark Twisted Fantasy is the fifth studio album by American rapper and producer Kanye West. It was released on November 22, 2010, by Def Jam Recordings and Roc-A-Fella Records following a period of public controversy for West.',
+      'genre': 'Hip-hop',
+      'category': 'Vinyl',
+      'price': 79.69,
+      'countInStock': 30,
+      'rating': 4,
+      'numReviews': 12,
+    },
+  ]
+  
+  
+  export default products
+  
